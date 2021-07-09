6b72f26df5d62eeb32795705d778a7afd85b97e7
GeorgeBell233 <87140880+GeorgeBell233@users.noreply.github.com>
2021-07-09 09:13:47 +0100

    Update Fizzbuzz using Functions.py

--- Fizzbuzz using Functions.py
+++ Fizzbuzz using Functions.py
@@ -8,13 +8,15 @@ def functionTest(num, division):
 for i in range(1,101):
     value = ""
     
+    #No need to check for a multiple of 3 and 5 as it will pass both tests and be concatenated as one string as "FizzBuzz"
+    
     if functionTest(i,3) == True:
         value += "Fizz"
     
     if functionTest(i,5) == True:
         value += "Buzz"
     
-    if value == "":
-        value=i
+    if value == "": #Rather than checking if it is not a multiple, checking the string is an easier way to do the same thing
+        value = i
     
     print(value)
